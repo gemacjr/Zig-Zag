a07a6e53933d2a4797373e092246fb3bb84a81d2
Ed <edmccormic@gmail.com>
2017-04-10 10:30:51 -0600

    Initial Commit

---
+++ Zig ZagTests/Zig_ZagTests.swift
@@ -0,0 +1,36 @@
+//
+//  Zig_ZagTests.swift
+//  Zig ZagTests
+//
+//  Created by Ed McCormic on 4/10/17.
+//  Copyright © 2017 Swiftbeard. All rights reserved.
+//
+
+import XCTest
+@testable import Zig_Zag
+
+class Zig_ZagTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
